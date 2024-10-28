becc80e7ecfd97dc7a316d10fcbae8ca461a16ec
Mussie Teshome <mussieteshomeg@gmail.com>
2024-10-28 13:36:22 +0300

    main file restructured

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express')
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const persons = require('./persons')
+const router = require('./router')
+
 const app = express()
 
-let persons = [{
-    id: '1',
-    name: 'Sam',
-    age: '26',
-    hobbies: []    
-}] //This is your in memory database
+// Test non existing user should be tested with Non existing user with id different from 1
+app.use(cors())
+app.use(bodyParser.json())
 
 app.set('db', persons)
-//TODO: Implement crud of person
+app.use(router)
+
+// Non existing endpoint
+app.use((req, res) => {
+    res.status(404).json({ message: 'Route not found' });
+});
 
 if (require.main === module) {
     app.listen(3000)
 }
+
 module.exports = app;
